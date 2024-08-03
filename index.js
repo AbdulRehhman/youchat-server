cf596c8ee39f5560d26674eb5a2e9cceb2f20a75
AbdulRehhman <me.abdulrehman761@gmail.com>
2024-08-03 21:37:48 +0500

    initial setup

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const databaseURL = process.env.DATABASE_URL;
+
+app.use(cors());
+
+const server = app.listen(port, () => {
+  console.log(`Server is running on port: http://localhost:${port}`);
+});
+
